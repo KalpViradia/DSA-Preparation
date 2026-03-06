5a887c4e54e01e225481c100e659a058edbd782d
Kalp Viradia <160616083+KalpViradia@users.noreply.github.com>
2026-03-06 19:45:02 +0530

    Refactor pattern printing logic in Question17.java

--- ProgramAskedInCompany/Question17.java
+++ ProgramAskedInCompany/Question17.java
@@ -9,31 +9,33 @@ public class Question17 {
         System.out.print("Enter pattern size n: ");
         int n = sc.nextInt();
 
-        for (int i = 1; i <= n; i++) {
+        int spaces = 2 * n - 1;
+        int stars = 0;
 
-            for (int j = 1; j <= i; j++)
-                System.out.print("* ");
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            if (i <= n) {
+                spaces = spaces - 2;
+                stars++;
+            }
 
-            for (int j = 1; j <= 2 * (n - i); j++)
-                System.out.print("  ");
+            else {
+                spaces = spaces + 2;
+                stars--;
+            }
 
-            for (int j = 1; j <= i; j++)
-                System.out.print("* ");
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
 
-            System.out.println();
-        }
-
-        for (int i = n; i >= 1; i--) {
-
-            for (int j = 1; j <= i; j++)
-                System.out.print("* ");
-
-            for (int j = 1; j <= 2 * (n - i); j++)
-                System.out.print("  ");
-
-            for (int j = 1; j <= i; j++)
-                System.out.print("* ");
-
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            
+            for (int j = 1; j <= stars; j++) {
+                if (j != n) {
+                    System.out.print("*");
+                }
+            }
             System.out.println();
         }
 
