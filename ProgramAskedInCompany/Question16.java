ef54a768f8aebd6c80a8d2882b140a183b2cec7b
Kalp Viradia <160616083+KalpViradia@users.noreply.github.com>
2026-03-06 19:36:38 +0530

    Update Question16.java

--- ProgramAskedInCompany/Question16.java
+++ ProgramAskedInCompany/Question16.java
@@ -1,35 +1,72 @@
+// import java.util.*;
+
+// public class Question16 {
+
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter array size: ");
+//         int n = sc.nextInt();
+//         int[] nums = new int[n];
+
+//         System.out.println("Enter " + n + " elements (values between 1 and " + (n-1) + "):");
+//         for (int i = 0; i < n; i++)
+//             nums[i] = sc.nextInt();
+
+//         int slow = nums[0];
+//         int fast = nums[0];
+
+//         do {
+//             slow = nums[slow];
+//             fast = nums[nums[fast]];
+//         } while (slow != fast);
+
+//         slow = nums[0];
+
+//         while (slow != fast) {
+//             slow = nums[slow];
+//             fast = nums[fast];
+//         }
+
+//         System.out.println(fast);
+
+//         sc.close();
+//     }
+// }
+
+
+// also works
+
 import java.util.*;
 
 public class Question16 {
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter array size: ");
         int n = sc.nextInt();
+
         int[] nums = new int[n];
 
-        System.out.println("Enter " + n + " array elements (values 1 to n-1):");
-        for (int i = 0; i < n; i++)
+        System.out.println("Enter " + n + " elements:");
+        for(int i = 0; i < n; i++){
             nums[i] = sc.nextInt();
-
-        int slow = nums[0];
-        int fast = nums[0];
-
-        do {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (slow != fast);
-
-        slow = nums[0];
-
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
         }
 
-        System.out.println(fast);
+        HashSet<Integer> set = new HashSet<>();
+        int duplicate = -1;
+
+        for(int num : nums){
+            if(set.contains(num)){
+                duplicate = num;
+                break;
+            }
+            set.add(num);
+        }
+
+        System.out.println("Repeated number: " + duplicate);
 
         sc.close();
     }
